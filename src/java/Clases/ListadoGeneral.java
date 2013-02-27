6dbb7771f8d34f4a0785689d435789ebf6e921c3
cheto <cheto@cheto-laptop>
2013-02-26 21:23:58 -0430

    Principio Listado General

---
+++ src/java/Clases/ListadoGeneral.java
@@ -0,0 +1,105 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import org.apache.struts.action.ActionForm;
+
+/**
+ *
+ * @author cheto
+ */
+public class ListadoGeneral extends ActionForm {
+    private String usuario;
+    private String equipo;
+    private String periodo;
+    private String tipo;
+    private String sexo;
+    private String organizadoPor;
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the equipo
+     */
+    public String getEquipo() {
+        return equipo;
+    }
+
+    /**
+     * @param equipo the equipo to set
+     */
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+
+    /**
+     * @return the periodo
+     */
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    /**
+     * @param periodo the periodo to set
+     */
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the organizadoPor
+     */
+    public String getOrganizadoPor() {
+        return organizadoPor;
+    }
+
+    /**
+     * @param organizadoPor the organizadoPor to set
+     */
+    public void setOrganizadoPor(String organizadoPor) {
+        this.organizadoPor = organizadoPor;
+    }
+    
+}
