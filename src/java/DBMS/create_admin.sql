4a42e9a6ecfcefa35317170b13b652c54215cbe1
daniel <daniel@daniel-Inspiron-1520>
2013-05-25 11:41:50 -0430

    create admin

--- src/java/DBMS/create_admin.sql
+++ src/java/DBMS/create_admin.sql
@@ -1,3 +1,3 @@
-INSERT INTO "PREPAS".usuario VALUES ('cheto@usb.ve','$2a$06$VPlSQ3I4V1qA9IqzY900Quq4s85qRvdg5MS70vZh8gdzYW90XAYXi','cheto1@usb.ve','cheto','rodriguez','19227483','','','DSI','masculino','S','S','28','S','36','true','administrador');
+INSERT INTO "PREPAS".usuario VALUES ('cheto@usb.ve','$2a$06$VPlSQ3I4V1qA9IqzY900Quq4s85qRvdg5MS70vZh8gdzYW90XAYXi','cheto1@usb.ve','Cheto','rodriguez','19227483','','','DSI','masculino','S','S','28','S','36','true','administrador');
 
 INSERT INTO "PREPAS".noticia VALUES ('cheto@usb.ve','Inician trabajos','Inician actividades de implementacion del 60% del sistema de la DSI ','2012-11-29');
