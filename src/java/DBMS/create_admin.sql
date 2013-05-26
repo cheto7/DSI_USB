9e3572501fdc05658f644ae191722d497a184fad
karen <karen@karen-K52F>
2013-05-26 19:20:01 -0430

    Recuperar contrasena sin email :) soy cool

--- src/java/DBMS/create_admin.sql
+++ src/java/DBMS/create_admin.sql
@@ -7,7 +7,6 @@ INSERT INTO "PREPAS".usuario VALUES ('azocar@usb.ve','$2a$06$VPlSQ3I4V1qA9IqzY90
 INSERT INTO "PREPAS".usuario VALUES ('karen@usb.ve','$2a$06$VPlSQ3I4V1qA9IqzY900Quq4s85qRvdg5MS70vZh8gdzYW90XAYXi','karen@usb.ve','Karen','Da Silva','19227485','','04262047676','DSI','femenino','S','S','28','S','36','true','inspector');
 INSERT INTO "PREPAS".usuario VALUES ('ivan@usb.ve','$2a$06$VPlSQ3I4V1qA9IqzY900Quq4s85qRvdg5MS70vZh8gdzYW90XAYXi','ivan@usb.ve','Ivan','Travecedo','19227486','','04262047676','DSI','masculino','S','S','28','S','36','true','supervisor');
 
-
 INSERT INTO "PREPAS".noticia VALUES ('cheto@usb.ve','Inician trabajos','Inician actividades de implementacion del 60% del sistema de la DSI ','2012-11-29');
 INSERT INTO "PREPAS".noticia VALUES ('mgomez@usb.ve','Entrega 30%','Ya se encuentra habilitado el sistema para la DSI con el 30% de los casos de uso implementados. ','2012-11-29');
 
